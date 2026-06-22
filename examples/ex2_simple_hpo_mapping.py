2fb81ff7a160ed607bd9f89a4426a7c110fd2143
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-06-21 22:17:04 -0300

    Example 2 modified to better show results

--- examples/ex2_simple_hpo_mapping.py
+++ examples/ex2_simple_hpo_mapping.py
@@ -2,8 +2,15 @@ from symptoms_recognizer.symptom_recognizer import SymptomRecognizer
 
 if __name__ == "__main__":
     recognizer = SymptomRecognizer()
-    symptoms_list = ["mareos", "náuseas"]
+    expected_results = {"Fiebre" : "HP:0001945", "Náusea" : "HP:0002018"}
 
-    results = recognizer.map(symptoms_list)
+    results = recognizer.map(expected_results.keys())
 
-    print(f"Resultados: {results}")
+    print("-------------------------------")
+    print(f"Se espera:")
+    print(f" - Fiebre => HP:0001945")
+    print(f" - Náusea => HP:0002018")
+    print("-------------------------------")
+    print(f"Resultados:")
+    for hpo_code in results:
+        print(f" - {hpo_code}")
