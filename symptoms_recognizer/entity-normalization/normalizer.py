c73fc660df1c86c21490964e62a7ae5e2be09ab5
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-08-16 00:02:13 -0300

    Initial normalizer structure made

--- symptoms_recognizer/entity-normalization/normalizer.py
+++ symptoms_recognizer/entity-normalization/normalizer.py
@@ -1,6 +1,16 @@
 class PhenotypesNormalizer:
-    def __init__(self):
-        pass
+    def __init__(self, ontology):
+        self.ontology = ontology
 
-    def normalize(self, phenotypes_list: list[str]):
-        pass
+    def normalize(self, phenotypes_list: list[str]) -> list[str]:
+        # Correct misspellings
+        correctly_spelled_phenotypes = self._correct_misspelings(phenotypes_list)
+
+        # Convert to ontology and return
+        return self._normalize_phenotypes_to_ontology(correctly_spelled_phenotypes)
+
+    def _correct_misspelings(self, phenotypes_list: list[str]) -> list[str]:
+        return []
+
+    def _normalize_phenotypes_to_ontology(self, phenotypes_list) -> list[str]:
+        return []
